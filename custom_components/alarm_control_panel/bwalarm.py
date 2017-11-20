9d52c6d5e4dc5aa4f305768b69e48e5c2b7c66f1
Greg Mills <gregory.mills@gmail.com>
2017-11-20 09:20:32 -0500

    alarm and sensor updates

--- custom_components/alarm_control_panel/bwalarm.py
+++ custom_components/alarm_control_panel/bwalarm.py
@@ -21,14 +21,16 @@ import homeassistant.components.alarm_control_panel as alarm
 import homeassistant.components.switch as switch
 import homeassistant.helpers.config_validation as cv
 
-CONF_HEADSUP   = 'headsup'
-CONF_IMMEDIATE = 'immediate'
-CONF_DELAYED   = 'delayed'
-CONF_IGNORE    = 'homemodeignore'
-CONF_NOTATHOME = 'notathome'
-CONF_OVERRIDE  = 'override'
-CONF_ALARM     = 'alarm'
-CONF_WARNING   = 'warning'
+#CONF_HEADSUP        = 'headsup'
+CONF_IMMEDIATE      = 'immediate'
+CONF_DELAYED        = 'delayed'
+CONF_IGNORE         = 'homemodeignore'
+CONF_NOTATHOME      = 'notathome'
+CONF_OVERRIDE       = 'override'
+CONF_PERIMETER_MODE = 'perimeter_mode'
+CONF_PERIMETER      = 'perimeter'
+CONF_ALARM          = 'alarm'
+CONF_WARNING        = 'warning'
 
 CONF_WARNING_COLOUR  = 'warning_colour'
 CONF_PENDING_COLOUR  = 'pending_colour'
@@ -38,10 +40,11 @@ CONF_ARMED_AWAY_COLOUR  = 'armed_away_colour'
 CONF_ARMED_HOME_COLOUR  = 'armed_home_colour'
 
 CONF_CLOCK = 'clock'
+CONF_WEATHER = 'weather'
 
 # Add a new state for the time after an delayed sensor and an actual alarm
 STATE_ALARM_WARNING = 'warning'
-
+STATE_ALARM_ARMED_PERIMETER = 'armed_perimeter'
 class Events(enum.Enum):
     ImmediateTrip = 1
     DelayedTrip   = 2
@@ -50,6 +53,7 @@ class Events(enum.Enum):
     Timeout       = 5
     Disarm        = 6
     Trigger       = 7
+    ArmPerimeter  = 8
     
 PLATFORM_SCHEMA = vol.Schema({
     vol.Required(CONF_PLATFORM):  'bwalarm',
@@ -59,19 +63,22 @@ PLATFORM_SCHEMA = vol.Schema({
     vol.Required(CONF_ALARM):     cv.entity_id,  # switch/group to turn on when alarming
     vol.Required(CONF_WARNING):   cv.entity_id,  # switch/group to turn on when warning
     vol.Optional(CONF_CODE):      cv.string,
-    vol.Optional(CONF_HEADSUP):   cv.entity_ids, # things to show as a headsup, not alarm on
+    #vol.Optional(CONF_HEADSUP):   cv.entity_ids, # things to show as a headsup, not alarm on
     vol.Optional(CONF_IMMEDIATE): cv.entity_ids, # things that cause an immediate alarm
     vol.Optional(CONF_DELAYED):   cv.entity_ids, # things that allow a delay before alarm
     vol.Optional(CONF_IGNORE): cv.entity_ids,  # things that we ignore when at home
     vol.Optional(CONF_NOTATHOME): cv.entity_ids,  # things that we ignore when at home BACKWARDS COMPAT
     vol.Optional(CONF_OVERRIDE): cv.entity_ids,  # sensors that can be ignored if open when trying to set alarm in away mode
+    vol.Optional(CONF_PERIMETER_MODE): cv.boolean,  # Enable perimeter mode?
+    vol.Optional(CONF_PERIMETER): cv.entity_ids,  # things monitored under perimeter mode
     vol.Optional(CONF_WARNING_COLOUR):   cv.string, # Custom colour of warning display
     vol.Optional(CONF_PENDING_COLOUR): cv.string, # Custom colour of pending display
     vol.Optional(CONF_DISARMED_COLOUR):   cv.string, # Custom colour of disarmed display
     vol.Optional(CONF_TRIGGERED_COLOUR): cv.string,  # Custom colour of triggered display
     vol.Optional(CONF_ARMED_AWAY_COLOUR): cv.string,  # Custom colour of armed away display
     vol.Optional(CONF_ARMED_HOME_COLOUR): cv.string,  # Custom colour of armed home display
-    vol.Optional(CONF_CLOCK): cv.boolean  # DIsplay clock on panel?
+    vol.Optional(CONF_CLOCK): cv.boolean,  # DIsplay clock on panel
+    vol.Optional(CONF_WEATHER): cv.boolean  # DIsplay weather on panel
 })
 
 _LOGGER = logging.getLogger(__name__)
@@ -94,8 +101,10 @@ class BWAlarm(alarm.AlarmControlPanel):
         self._delayed      = set(config.get(CONF_DELAYED, []))
         self._ignore       = set(config.get(CONF_IGNORE, []) if config.get(CONF_IGNORE, []) != [] else config.get(CONF_NOTATHOME, []))
         self._override      = set(config.get(CONF_OVERRIDE, []))
-        self._allinputs    = self._immediate | self._delayed | self._ignore
-        self._allsensors   = self._allinputs | set(config.get(CONF_HEADSUP, []))
+        self._perimeter_mode = config.get(CONF_PERIMETER_MODE, False)
+        self._perimeter      = set(config.get(CONF_PERIMETER, []))
+        self._allsensors   = self._immediate | self._delayed | self._ignore
+        #self._allsensors   = self._allinputs 
         self._alarm        = config[CONF_ALARM]
         self._warning      = config[CONF_WARNING]
         self._code         = config[CONF_CODE] if config[CONF_CODE] else None
@@ -117,6 +126,7 @@ class BWAlarm(alarm.AlarmControlPanel):
         self._armed_home_colour = config.get(CONF_ARMED_HOME_COLOUR, 'black')
        
         self._clock = config.get(CONF_CLOCK, False)
+        self._weather = config.get(CONF_WEATHER, False)
 
         self.clearsignals()
 
@@ -133,11 +143,13 @@ class BWAlarm(alarm.AlarmControlPanel):
     @property
     def device_state_attributes(self):
         return {
-            'immediate':  sorted(list(self.immediate)),
-            'delayed':    sorted(list(self.delayed)),
+            'immediate':  sorted(list(self._immediate)),
+            'delayed':    sorted(list(self._delayed)),
             'override':   sorted(list(self._override)),
             'ignored':    sorted(list(self.ignored)),
             'allsensors': sorted(list(self._allsensors)),
+            'perimeter_mode':    self._perimeter_mode,
+            'perimeter':    sorted(list(self._perimeter)),
             'changedby':  self.changed_by,
             'warning_colour':  self._warning_colour,
             'pending_colour':  self._pending_colour,
@@ -146,7 +158,8 @@ class BWAlarm(alarm.AlarmControlPanel):
             'armed_home_colour':  self._armed_home_colour,
             'armed_away_colour':  self._armed_away_colour,
             'countdown_time':  self._countdown_time,
-            'clock':  self._clock
+            'clock':  self._clock,
+            'weather':  self._weather
         }
 
 
@@ -162,7 +175,7 @@ class BWAlarm(alarm.AlarmControlPanel):
     def state_change_listener(self, event):
         """ Something changed, we only care about things turning on at this point """
         new = event.data.get('new_state', None)
-        if new is None or new.state != STATE_ON:
+        if new is None or new.state.lower() != STATE_ON:
             return
         eid = event.data['entity_id']
         if eid in self.immediate:
@@ -193,35 +206,36 @@ class BWAlarm(alarm.AlarmControlPanel):
         #    return
         self.process_event(Events.ArmAway)
 
+    def alarm_arm_night(self, code=None):
+        self.process_event(Events.ArmPerimeter)
+
     def alarm_trigger(self, code=None):
         self.process_event(Events.Trigger)
 
 
     ### Internal processing
 
-    def noton(self, eid):
-        """ For filtering out sensors already tripped """
-        return not self._hass.states.is_state(eid, STATE_ON)
-
-    def setsignals(self, athome):
+    def setsignals(self, alarmMode):
         """ Figure out what to sense and how """
-        #self.immediate = set(filter(self.noton, self._immediate))
-        self.immediate = self._immediate
-        #self.delayed = set(filter(self.noton, self._delayed))
-        self.delayed = self._delayed
-        if athome:
+        if alarmMode == Events.ArmHome or alarmMode == Events.ArmAway:
+            self.immediate = self._immediate
+            self.delayed = self._delayed
+        if alarmMode == Events.ArmHome:
             self.immediate -= self._ignore
             self.delayed -= self._ignore
-        self.ignored = self._allinputs - (self.immediate | self.delayed)
+        if alarmMode == Events.ArmPerimeter:
+           self.immediate = self._perimeter
+        self.ignored = self._allsensors - (self.immediate | self.delayed)
 
     def clearsignals(self):
         """ Clear all our signals, we aren't listening anymore """
         self.immediate = set()
         self.delayed = set()
-        self.ignored = self._allinputs.copy()
+        self.ignored = self._allsensors.copy()
 
     def process_event(self, event):
         """ 
+           WARNING THIS CODE HAS CHANGED. NOTE LIKELY INCORRECT. 
            This is the core logic function.
            The possible states and things that can change our state are:
                  Actions:  isensor dsensor timeout arm_home arm_away disarm trigger
@@ -244,10 +258,12 @@ class BWAlarm(alarm.AlarmControlPanel):
         elif old == STATE_ALARM_DISARMED:
             if   event == Events.ArmHome:       self._state = STATE_ALARM_ARMED_HOME
             elif event == Events.ArmAway:       self._state = STATE_ALARM_PENDING
+            elif event == Events.ArmPerimeter:       self._state = STATE_ALARM_ARMED_PERIMETER
         elif old == STATE_ALARM_PENDING:
             if   event == Events.Timeout:       self._state = STATE_ALARM_ARMED_AWAY
         elif old == STATE_ALARM_ARMED_HOME or \
-             old == STATE_ALARM_ARMED_AWAY:
+             old == STATE_ALARM_ARMED_AWAY or \
+             old == STATE_ALARM_ARMED_PERIMETER:
             if   event == Events.ImmediateTrip: self._state = STATE_ALARM_TRIGGERED
             elif event == Events.DelayedTrip:   self._state = STATE_ALARM_WARNING
         elif old == STATE_ALARM_WARNING:
@@ -272,13 +288,20 @@ class BWAlarm(alarm.AlarmControlPanel):
                 switch.turn_on(self._hass, self._warning)
                 self._timeoutat = now() + self._pending_time
                 self._returnto = STATE_ALARM_ARMED_AWAY
-                self.setsignals(False)
+                #self.setsignals(False)
+                self.setsignals(Events.ArmAway)
             elif new == STATE_ALARM_ARMED_HOME:
                 self._returnto = new
-                self.setsignals(True)
+                #self.setsignals(True)
+                self.setsignals(Events.ArmHome)
             elif new == STATE_ALARM_ARMED_AWAY:
                 self._returnto = new
-                self.setsignals(False)
+                #self.setsignals(False)
+                self.setsignals(Events.ArmAway)
+            elif new == STATE_ALARM_ARMED_PERIMETER:
+                self._returnto = new
+                #self.setsignals(False)
+                self.setsignals(Events.ArmPerimeter)
             elif new == STATE_ALARM_DISARMED:
                 self._returnto = new
                 self.clearsignals()
